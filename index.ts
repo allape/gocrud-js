bb3e58e0e456e3279c8702557d3305687afa212f
as <allensnape@gmail.com>
2025-02-13 12:11:34 +0800

    chore: refine i18n

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ export * from "./src/fetcheese";
 export * from "./src/model";
 export * from "./src/sha256";
 
+export * as i18n from "./src/i18n";
+
 import Crudy from "./src/index";
 
 export default Crudy;
